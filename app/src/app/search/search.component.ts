08d452c0c941881a0472e3d59381314f0fcbe1cf
arjunkashyap <arjun.kashyap@yahoo.co.in>
2018-08-27 14:59:03 +0530

    soft jeyboard added

--- app/src/app/search/search.component.ts
+++ app/src/app/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
+import * as _underscore from 'underscore';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,12 +12,13 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class SearchComponent implements OnInit {
 
   search:Array<any>;
-  title:String;
-  authornames:String;
-  feature:String;
-  series:String;
-  fulltext:String;
-  year:String;
+  title:string = '';
+  authornames:string = '';
+  feature:string = '';
+  series:string = '';
+  fulltext:string = '';
+  year:string = '';
+  elid:string;
   
   constructor( private route: ActivatedRoute, private router: Router ) { }
 
@@ -23,6 +26,43 @@ export class SearchComponent implements OnInit {
 
   onSubmit(form: any) {
 
+    form = _underscore.pick(form, _underscore.identity;
 	  this.router.navigate(['/searchResults'], { queryParams:  form });
+  }
+
+  onFocus(field: string) {
+
+    this.elid = field;
+    console.log(this.elid);
+  }
+
+  insertText(text: string) {
+
+     switch (this.elid) {
+
+      case 'title' :
+        
+        this.title = (this.title) ? this.title + text : text;
+        break;
+
+      case 'authornames' :
+        
+        this.authornames = (this.authornames) ? this.authornames + text : text;
+        break;
+
+      case 'feature' :
+        
+        this.feature = (this.feature) ? this.feature + text : text;
+        break;
+
+      case 'series' :
+        
+        this.series = (this.series) ? this.series + text : text;
+        break;
+
+      case 'fulltext' :
+        
+        this.fulltext = (this.fulltext) ? this.fulltext + text : text;
+        break;
   }
 }
